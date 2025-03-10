b5eeab4b4c108d68b5a9a51796b1bb43e4c368fd
joe <joseph.avery.cox@gmail.com>
2025-03-10 14:33:19 -0400

    fixs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    server: {
+      host: '0.0.0.0',
+      port: 4321,
+      allowedHosts: ['psikrotic.duckdns.org']
+    }
+  }
+});
